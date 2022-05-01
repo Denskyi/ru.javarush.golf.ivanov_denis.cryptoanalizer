f0db2613723f1e16afdbbb2efcddc05f8c74d3f9
denskyi <bio.rs@yandex.ru>
2022-05-02 01:45:04 +0200

    Добавлен обработчек выбора числа Добавлено автоматическое формирования HashMap для шифровки/дишифровки

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,29 @@
-public class Runner {
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Runner {//https://github.com/Denskyi/ru.javarush.golf.ivanov_denis.cryptoanalizer.git
     public static void main(String[] args) {
-        System.out.println("test");
+        int offset = reading_number();
+        LetterMap map = new LetterMap();
+        HashMap<Character, Character> map1 = map.getMap(offset);
+        try {
+
+        } catch (Exception e) {
+        }
+
+    }
+
+    public static int reading_number() {
+        while (true) {
+            System.out.println("Введите число от 1 до 31");
+            int read;
+            try (Scanner scanner = new Scanner(System.in);) {
+                read = Integer.parseInt(scanner.nextLine());
+                if (read > 0 && read < 32) {break;
+                } else {System.out.println("Число вне диапазона");}
+            } catch (Exception e) {
+                System.out.println("Введено не число");}
+        }
+        return 1;
     }
 }
