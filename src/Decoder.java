fed10fd258907426800bf3cc47aefeca3d9ad1ec
denskyi <bio.rs@yandex.ru>
2022-05-04 03:23:27 +0200

    HashMap больше не нужна, кодирование и декодирование происходит только смещением относительно места символа в UTF-8 Добавлен дешифратор

--- src/Decoder.java
+++ src/Decoder.java
@@ -1,27 +1,26 @@
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class Encoder{
+public class Decoder {
     private String unencryptedFile = "/home/denskyi/IdeaProjects/ru.javarush.golf.ivanov_denis.cryptoanalizer/src/unencryptedFile";
     private String encryptedFile = "/home/denskyi/IdeaProjects/ru.javarush.golf.ivanov_denis.cryptoanalizer/src/encryptedFile";
-    public Encoder(int offset) {
-        try (FileReader readFile = new FileReader(unencryptedFile);
-             FileWriter writeFile = new FileWriter(encryptedFile);){
-            while (readFile.ready()) {//шифруется абсолютно любой символ
-                int read = readFile.read() + offset;
+
+    public Decoder(int offset) {
+        try (FileReader readFile = new FileReader(encryptedFile);
+             FileWriter writeFile = new FileWriter(unencryptedFile);) {
+            while (readFile.ready()) {//Дешифруется по выбранному параметру offset абсолютно любой символ
+                int read = readFile.read() - offset;
                 if (read > 99999) {
                     read -= 99999;
                 } else if (read < 0) {
                     read += 99999;
                 } else {
-                    writeFile.write((char)read);
+                    writeFile.write((char) read);
                 }
             }
         } catch (IOException e) {
             System.out.println("Exceptions");
         }
     }
-
-
-
 }
-
--- src/Runner.java
+++ src/Runner.java
@@ -3,27 +3,34 @@ import java.util.Scanner;
 
 public class Runner {//https://github.com/Denskyi/ru.javarush.golf.ivanov_denis.cryptoanalizer.git
     public static void main(String[] args) {
+        /*Запасной не кодированный текст
+Мы созданы нашими генами.
+Мы, животные, существуем, чтобы сохранить их, и служим лишь машинами, обеспечивающими их выживание.
+Мир эгоистичного гена — это мир жестокой конкуренции, безжалостной эксплуатации и обмана.
+Ну а как же акты альтруизма, наблюдаемые в природе: пчелы, совершающие самоубийство, когда они жалят врага, чтобы защитить улей, или птицы, рискующие жизнью, чтобы предупредить стаю о приближении ястреба?
+Противоречит ли это фундаментальному закону об эгоистичности гена?
+Ни в коем случае! Докинз показывает, что эгоистичный ген — это еще и хитрый ген.
+И он лелеет надежду, что вид Homo sapiens — единственный на всем земном шаре — в силах взбунтоваться против намерений эгоистичного гена.
+Перевод сверен по юбилейному английскому изданию 2006 года.
+         */
+        System.out.println("Введите число от 0 до 99999");
         int offset = reading_number();
+        Encoder encoder = new Encoder(offset);
+        Decoder decoder = new Decoder(offset);
         LetterMap map = new LetterMap();
         HashMap<Character, Character> map1 = map.getMap(offset);
-        try {
-
-        } catch (Exception e) {
-        }
 
     }
 
     public static int reading_number() {
+        int read;
         while (true) {
-            System.out.println("Введите число от 1 до 31");
-            int read;
             try (Scanner scanner = new Scanner(System.in);) {
-                read = Integer.parseInt(scanner.nextLine());
-                if (read > 0 && read < 32) {break;
-                } else {System.out.println("Число вне диапазона");}
+                read = (Integer.parseInt(scanner.nextLine()) % 99999);//%99999 Должен убрать возможность выйти из диапазона UTF8 более одного раза
+                break;
             } catch (Exception e) {
                 System.out.println("Введено не число");}
         }
-        return 1;
+        return read;
     }
 }
